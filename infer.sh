9a357ee786c3d29795ccbb4c5246d10833e006c8
Elena Gensch <elena.gensch@student.hpi.uni-potsdam.de>
2023-02-26 18:38:43 +0100

    Adjust bash script for args and placeholders

--- infer.sh
+++ infer.sh
@@ -5,10 +5,13 @@
 #SBATCH --partition=<PARTITION_NAME> # -p
 #SBATCH --account=<ACCOUNT_NAME> # -A
 #SBATCH --mem-per-cpu=<NUMBER>G
+#SBATCH --gpus=1
+
 
 eval "$(conda shell.bash hook)"
 set +eu
-source activate <ENV>
+source activate blip
 set +eu
 
-python predict.py
+# add optional arguments: --max_length=<integer>, --num_captions=<int>, --model_type=<str>, --use_nucleus_sampling=<bool>
+python predict.py --file_path=<str - path to image> -force_words <str list, e.g: 'a' 'wood'>
