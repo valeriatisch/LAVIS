b80c14eabddafaecef80bf0f08746649baf41488
Valeria Tisch <valeria.tisch97@gmail.com>
2023-02-26 16:44:29 +0100

    added placeholder

--- infer.sh
+++ infer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eux
 #SBATCH --job-name=captioning-blip
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=elena.gensch@student.hpi.uni-potsdam.de
-#SBATCH --partition=magic # -p
-#SBATCH --account=naumann-mpws2022fn1 # -A
-#SBATCH --mem-per-cpu=32G
+#SBATCH --mail-user=<MAIL>
+#SBATCH --partition=<PARTITION_NAME> # -p
+#SBATCH --account=<ACCOUNT_NAME> # -A
+#SBATCH --mem-per-cpu=<NUMBER>G
 
 eval "$(conda shell.bash hook)"
 set +eu
-source activate lavis-blip
+source activate <ENV>
 set +eu
 
 python predict.py
