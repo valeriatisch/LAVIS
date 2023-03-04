181dba6bc1e8911b67cfc7f11615dd76e42f4ff6
sm1lla <foxsmilla@gmail.com>
2023-03-04 14:05:58 +0000

    remove unnecessary file and format

--- lavis/datasets/datasets/artpedia_artist.py
+++ lavis/datasets/datasets/artpedia_artist.py
@@ -1,16 +1,7 @@
-"""
- Copyright (c) 2022, salesforce.com, inc.
- All rights reserved.
- SPDX-License-Identifier: BSD-3-Clause
- For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
-"""
-import logging
 import os
-import json
-from collections import OrderedDict
-
 from lavis.datasets.datasets.coco_caption_datasets import COCOCapEvalDataset
 from PIL import Image
+
 Image.MAX_IMAGE_PIXELS = None
 
 
@@ -25,17 +16,18 @@ class ArtistEvalDataset(COCOCapEvalDataset):
         """
         super().__init__(vis_processor, text_processor, vis_root, ann_paths)
 
-        #Build artist set including all artists
+        # Build artist set including all artists
         artists = set()
         for ann in self.annotation:
-            ann['artist'] = [name.lower() for name in ann['artist']]
-            artists.update(ann['artist'])
+            ann["artist"] = [name.lower() for name in ann["artist"]]
+            artists.update(ann["artist"])
         self.artists = list(artists)
 
         # Format reference: add empty strings for not mentioned artists in the artist set
         for ann in self.annotation:
-            ann['artist'] = [artist if artist in ann['artist'] else '' for artist in self.artists]
-
+            ann["artist"] = [
+                artist if artist in ann["artist"] else "" for artist in self.artists
+            ]
 
     def __getitem__(self, index):
         ann = self.annotation[index]
