b7fc61682d63fbf85668b23ba5beb96886276649
sm1lla <foxsmilla@gmail.com>
2023-03-04 17:43:14 +0000

    rename variables in filtered dataset

--- lavis/datasets/datasets/artpedia_filtered_dataset.py
+++ lavis/datasets/datasets/artpedia_filtered_dataset.py
@@ -2,12 +2,17 @@ from .coco_caption_datasets import CaptionDataset, CaptionEvalDataset
 
 
 class FilteredArtpediaDataset(CaptionDataset):
-    def __init__(self, vis_processor, text_processor, vis_root, ann_paths, filtering_threshold):
-        """
-        vis_root (string): Root directory of images (e.g. coco/images/)
-        ann_root (string): directory to store the annotation file
-        """
-        super().__init__(vis_processor, text_processor, vis_root, ann_paths)
+    def __init__(
+        self,
+        vis_processor,
+        text_processor,
+        image_directory,
+        annotations_path,
+        filtering_threshold,
+    ):
+        super().__init__(
+            vis_processor, text_processor, image_directory, annotations_path
+        )
         filtered_annotations = []
         for annotation in self.annotation:
             if annotation["matching_score"] >= filtering_threshold:
@@ -16,12 +21,17 @@ class FilteredArtpediaDataset(CaptionDataset):
 
 
 class FilterdArtpediaEvalDataset(CaptionEvalDataset):
-    def __init__(self, vis_processor, text_processor, vis_root, ann_paths, filtering_threshold):
-        """
-        vis_root (string): Root directory of images (e.g. coco/images/)
-        ann_root (string): directory to store the annotation file
-        """
-        super().__init__(vis_processor, text_processor, vis_root, ann_paths)
+    def __init__(
+        self,
+        vis_processor,
+        text_processor,
+        image_directory,
+        annotations_path,
+        filtering_threshold,
+    ):
+        super().__init__(
+            vis_processor, text_processor, image_directory, annotations_path
+        )
         filtered_annotations = []
         for annotation in self.annotation:
             if annotation["matching_score"] >= filtering_threshold:
